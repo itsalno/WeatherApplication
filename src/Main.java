9c3521d4b96d0a49c3710cec8fb8ecf3e884abed
alekss <anoviconoks@gmail.com>
2024-02-07 23:33:05 +0100

    Changed the design and added pictures

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("MainView.fxml"));
+                getClass().getResource("WeatherGUI.fxml"));
         Parent root = loader.load();
         primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 }
