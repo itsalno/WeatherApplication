26a7d5ce4e98c0a0124d50b4a614422b79c0c2cc
alekss <anoviconoks@gmail.com>
2024-02-07 18:23:05 +0100

    First structure and "on-eye" design of the application

---
+++ src/GUI/Controllers/MSController.java
@@ -0,0 +1,43 @@
+package GUI.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MSController implements Initializable {
+
+    @FXML
+    private Label datelbl;
+    @FXML
+    private Label citycountrylbl;
+    @FXML
+    private Label degreeslbl;
+    @FXML
+    private Label humiditylbl;
+    @FXML
+    private Label windlbl;
+    @FXML
+    private Label firstdaylbl;
+    @FXML
+    private Label scnddaylbl;
+    @FXML
+    private Label thrdaylbl;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    public void changelocation(ActionEvent actionEvent) {
+
+    }
+
+
+
+}
